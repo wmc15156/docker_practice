366fb24c72b79a6ed3de909a9327a9dcebd0392c
HyjinKimDev <hjkim@itemmoa.co>
2020-10-29 13:41:05 +0900

    test

---
+++ 2020-10-20_2/build/precache-manifest.b5258a821a2979ab6aef39ef31a0f950.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4d23b6a1bc7266a80e4fcbb9355371",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19299fd61e92d0fa11b2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c3c38428b67eb802e515",
+    "url": "/static/js/2.3f7d1130.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3f7d1130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19299fd61e92d0fa11b2",
+    "url": "/static/js/main.410dda37.chunk.js"
+  },
+  {
+    "revision": "5441cc4d06ac4486ea3e",
+    "url": "/static/js/runtime-main.1ffe42ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
